eb84a53d41e75f518da66b1917ce2a5978575e8c
Adenilson Ribeiro <134528770+aaadjrbr@users.noreply.github.com>
2023-07-04 13:29:18 -0300

    updated version

--- public/js/script.js
+++ public/js/script.js
@@ -1,17 +1,19 @@
 // Define an array of student objects
 const students = [
   {
-    name: "Adenilson Ribeiro",
+    name: "John Doe",
     number: "S1234567",
     paymentStatus: "Paid",
-    homework: "Math assignment due on 5th July",
+    homeworkText: "Math assignment due on 5th July",
+    homeworkLinkText: "View Assignment",
     nextClass: "Science class on 7th July at 10:00 AM"
   },
   {
-    name: "Marcia Das Graças",
+    name: "Jane Smith",
     number: "S2345678",
     paymentStatus: "Not Paid",
-    homework: "English essay due on 6th July",
+    homeworkText: "English essay due on 6th July",
+    homeworkLinkText: "View Assignment",
     nextClass: "History class on 8th July at 9:30 AM"
   },
   // Add more student objects here as needed
@@ -20,32 +22,44 @@ const students = [
 // Handle form submission
 const searchForm = document.getElementById('searchForm');
 const studentInfo = document.getElementById('studentInfo');
+const searchInput = document.getElementById('studentName');
 
 searchForm.addEventListener('submit', e => {
   e.preventDefault();
-  const searchTerm = document.getElementById('studentName').value.trim().toLowerCase();
+  const searchTerm = searchInput.value.trim().toLowerCase();
 
-  // Search for the student in the students array by name (partial match)
-  const matchedStudents = students.filter(student => {
-    const lowerCaseName = student.name.toLowerCase();
-    return lowerCaseName.includes(searchTerm);
-  });
-
-  if (matchedStudents.length > 0) {
-    // Display the matched student information
-    studentInfo.innerHTML = '';
-    matchedStudents.forEach(student => {
-      studentInfo.innerHTML += `
-        <h3>${student.name}</h3>
-        <p><strong>Number:</strong> ${student.number}</p>
-        <p><strong>Payment Status:</strong> ${student.paymentStatus}</p>
-        <p><strong>Homework:</strong> ${student.homework}</p>
-        <p><strong>Next Class:</strong> ${student.nextClass}</p>
-        <hr>
-      `;
+  if (searchTerm.length >= 3) {
+    // Search for the student in the students array by name (partial match)
+    const matchedStudents = students.filter(student => {
+      const lowerCaseName = student.name.toLowerCase();
+      return lowerCaseName.includes(searchTerm);
     });
+
+    if (matchedStudents.length > 0) {
+      // Display the matched student information
+      studentInfo.innerHTML = '';
+      matchedStudents.forEach(student => {
+        studentInfo.innerHTML += `
+          <h3>${student.name}</h3>
+          <p><strong>Number:</strong> ${student.number}</p>
+          <p><strong>Payment Status:</strong> ${student.paymentStatus}</p>
+          <p><strong>Homework:</strong> ${student.homeworkText} <a href="${getHomeworkLink(student)}">${student.homeworkLinkText}</a></p>
+          <p><strong>Next Class:</strong> ${student.nextClass}</p>
+          <hr>
+        `;
+      });
+    } else {
+      // No matching students found
+      studentInfo.innerHTML = '<p>No matching students found.</p>';
+    }
   } else {
-    // No matching students found
-    studentInfo.innerHTML = '<p>No matching students found.</p>';
+    // Clear student information if search term is less than three letters
+    studentInfo.innerHTML = '<p>Please enter at least three letters.</p>';
   }
 });
+
+function getHomeworkLink(student) {
+  // Here, you can dynamically generate the link based on the student's information.
+  // For demonstration purposes, we'll simply return a placeholder link.
+  return `https://example.com/homework/${student.number}`;
+}
