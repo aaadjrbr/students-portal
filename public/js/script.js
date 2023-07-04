e50d785d75e9ccfe401bf84afb1a6f94cac492e9
Adenilson Ribeiro <134528770+aaadjrbr@users.noreply.github.com>
2023-07-04 16:55:09 -0300

    update

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,3 @@
-// Define an array of student objects
 const students = [
   {
     name: "John Doe",
@@ -6,7 +5,10 @@ const students = [
     paymentStatus: "Paid",
     homeworkText: "Math assignment due on 5th July",
     homeworkLinkText: "View Assignment",
-    nextClass: "Science class on 7th July at 10:00 AM"
+    nextClass: "Science class on 7th July at 10:00 AM",
+    fixedTime: "Monday and Friday from 5:00 PM to 6:00 PM",
+    rescheduledClass: "No rescheduled class",
+    paymentDue: "No payment due"
   },
   {
     name: "Jane Smith",
@@ -14,7 +16,10 @@ const students = [
     paymentStatus: "Not Paid",
     homeworkText: "English essay due on 6th July",
     homeworkLinkText: "View Assignment",
-    nextClass: "History class on 8th July at 9:30 AM"
+    nextClass: "History class on 8th July at 9:30 AM",
+    fixedTime: "Tuesday and Thursday from 4:00 PM to 5:30 PM",
+    rescheduledClass: "Math class on 9th July at 11:00 AM",
+    paymentDue: "Payment due on 10th July"
   },
   // Add more student objects here as needed
 ];
@@ -39,12 +44,26 @@ searchForm.addEventListener('submit', e => {
       // Display the matched student information
       studentInfo.innerHTML = '';
       matchedStudents.forEach(student => {
+        // Determine the text color based on payment status
+        const textColor = student.paymentStatus === 'Paid' ? 'green' : 'red';
+
+        // Determine if the student has a rescheduled class
+        const hasRescheduledClass = student.rescheduledClass !== 'No rescheduled class';
+
+        // Check if the payment status is "Not Paid" and display an alert message
+        if (student.paymentStatus === 'Not Paid') {
+          alert(`Please contact me regarding the payment status of ${student.name}.`);
+        }
+
         studentInfo.innerHTML += `
           <h3>${student.name}</h3>
           <p><strong>Number:</strong> ${student.number}</p>
-          <p><strong>Payment Status:</strong> ${student.paymentStatus}</p>
+          <p><strong>Payment Status:</strong> <span style="color: ${textColor}">${student.paymentStatus}</span></p>
           <p><strong>Homework:</strong> ${student.homeworkText} <a href="${getHomeworkLink(student)}">${student.homeworkLinkText}</a></p>
           <p><strong>Next Class:</strong> ${student.nextClass}</p>
+          <p><strong>Fixed Time:</strong> ${student.fixedTime}</p>
+          <p class="${hasRescheduledClass ? 'rescheduled' : ''}"><strong>Rescheduled Class:</strong> ${student.rescheduledClass}</p>
+          <p><strong>Payment Due:</strong> ${student.paymentDue}</p>
           <hr>
         `;
       });
