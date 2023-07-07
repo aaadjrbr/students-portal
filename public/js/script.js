16ba48c171ecdbb7906481b43e829c3527e79641
Adenilson Ribeiro <134528770+aaadjrbr@users.noreply.github.com>
2023-07-07 16:12:46 -0300

    chainging stuff

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,16 @@
 const students = [
   {
+    name: "Gabriel Henrique Fernandes Cardoso",
+    number: "-",
+    paymentStatus: "Paid",
+    homeworkText: "Math assignment due on 5th July",
+    homeworkLinkText: "",
+    fixedTime: "Wednesday (6:00PM to 7:00PM) and Friday (5:00PM to 6:00PM)",
+    rescheduledClass: "No rescheduled class",
+    paymentDue: "No payment due<br/><br/><strong>Monthly Payment Date:</strong> 30th",
+    birthday: "2003-01-20" // Example birthday date, replace with actual birthday
+  },
+  {
     name: "Jane Smith",
     number: "S2345678",
     paymentStatus: "Not Paid",
@@ -11,70 +22,15 @@ const students = [
     birthday: "2001-05-23" // Example birthday date, replace with actual birthday
   },
   {
-    name: "Gabriel Henrique Fernandes Cardoso", //GABRIEL 1
-    number: "-",
+    name: "Carolina Assis Rodrigues",
+    number: "S24878",
     paymentStatus: "Paid",
-    homeworkText: "",
+    homeworkText: "English presentation due on 8th July",
     homeworkLinkText: "",
-    fixedTime: "Wednesday (6:00PM to 7:00PM) and Friday (5:00PM to 6:00PM)",
+    fixedTime: "Friday and Saturday from 4:00 PM to 5:30 PM",
     rescheduledClass: "No rescheduled class",
-    paymentDue: "No payment due<br/><br/><strong>Monthly Payment Date:</strong> 30th",
-    birthday: "2003-01-20" // Example birthday date, replace with actual birthday
-  },
-  {
-    name: "Adriano Alvim Rocha", //ADRIANO 2
-    number: "-",
-    paymentStatus: "Paid",
-    homeworkText: "",
-    homeworkLinkText: "",
-    fixedTime: "Monday (7:30PM to 8:30PM) and Thursday (7:30PM to 8:30PM)",
-    rescheduledClass: "Rescheduled class on Friday 7th",
-    paymentDue: "No payment due<br/><br/><strong>Monthly Payment Date:</strong> 4th",
-    birthday: "1995-07-29" // Example birthday date, replace with actual birthday
-  },
-  {
-    name: "Thiago Monteiro da Rocha", //THIAGO 3
-    number: "-",
-    paymentStatus: "Paid",
-    homeworkText: "",
-    homeworkLinkText: "",
-    fixedTime: "Tuesday (10:00AM to 11:00AM) and Thursday (10:00AM to 11:00AM)",
-    rescheduledClass: "No rescheduled class",
-    paymentDue: "No payment due<br/><br/><strong>Monthly Payment Date:</strong> 6th",
-    birthday: "1985-12-20" // Example birthday date, replace with actual birthday
-  },
-  {
-    name: "Sarah Gomes Da Trindade",  //SARAH 4
-    number: "-",
-    paymentStatus: "Not Paid",
-    homeworkText: "",
-    homeworkLinkText: "",
-    fixedTime: "Monday (8:30PM to 9:30PM) and Thursday (8:30PM to 9:30PM)",
-    rescheduledClass: "No rescheduled class",
-    paymentDue: "No payment due<br/><br/><strong>Monthly Payment Date:</strong> 10th",
-    birthday: "1988-12-26" // Example birthday date, replace with actual birthday
-  },
-  {
-    name: "Rute dos Santos Silva", //RUTE 5
-    number: "-",
-    paymentStatus: "Paid",
-    homeworkText: "",
-    homeworkLinkText: "",
-    fixedTime: "Wednesday (8:30PM to 9:30PM) and Saturday (1:00PM to 2:00PM)",
-    rescheduledClass: "No rescheduled class",
-    paymentDue: "No payment due<br/><br/><strong>Monthly Payment Date:</strong> 30th",
-    birthday: "1985-12-20" // Example birthday date, replace with actual birthday
-  },
-  {
-    name: "Letícia Rocha Souza",  //LETICIA 6
-    number: "-",
-    paymentStatus: "Not Paid",
-    homeworkText: "",
-    homeworkLinkText: "",
-    fixedTime: "Tuesday (7:30PM to 8:30PM) and Wednesday (7:30PM to 8:30PM)",
-    rescheduledClass: "No rescheduled class",
-    paymentDue: "No payment due<br/><br/><strong>Monthly Payment Date:</strong> 10th",
-    birthday: "1985-12-20" // Example birthday date, replace with actual birthday
+    paymentDue: "Payment due on 10th July",
+    birthday: "1997-04-08" // Example birthday date, replace with actual birthday
   },
   // Add more student objects here as needed
 ];
@@ -189,4 +145,17 @@ menuItems.forEach((menuItem, index) => {
       page.style.display = 'block';
     }
   });
+});
+
+
+const openVideoButton = document.getElementById('openVideoButton');
+const closeVideoButton = document.getElementById('closeVideoButton');
+const videoPopup = document.getElementById('videoPopup');
+
+openVideoButton.addEventListener('click', () => {
+  videoPopup.style.display = 'block';
+});
+
+closeVideoButton.addEventListener('click', () => {
+  videoPopup.style.display = 'none';
 });
