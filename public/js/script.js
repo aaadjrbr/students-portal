f9803b0274b370ca1cabf6cbfc3855d6ad98a954
Adenilson Ribeiro <134528770+aaadjrbr@users.noreply.github.com>
2023-09-20 07:17:58 -0300

    students-info

--- public/js/script.js
+++ public/js/script.js
@@ -21,7 +21,7 @@ const students = [
     homeworkText: "",
     homeworkLinkText: "Stuff we saw in our classes",
     fixedTime: "Wednesday (6:00PM to 7:00PM) and Friday (5:00PM to 6:00PM)",
-    rescheduledClass: "[4 Pending Classes]",
+    rescheduledClass: "[5 Pending Classes]",
     paymentDue: "No payment due<br/><br/><strong>Monthly Payment Date:</strong>-",
     birthday: "2003-01-20",
     getHomeworkLink: function() {
@@ -36,7 +36,7 @@ const students = [
     homeworkText: "",
     homeworkLinkText: "Stuff we saw in our classes",
     fixedTime: "Monday (7:30PM to 8:30PM) and Thursday (7:30PM to 8:30PM)",
-    rescheduledClass: "[9 Pending Classes]",
+    rescheduledClass: "[10 Pending Classes] + 1",
     paymentDue: "No payment due<br/><br/><strong>Monthly Payment Date:</strong> 4th",
     birthday: "1995-07-29",
     getHomeworkLink: function() {
@@ -81,7 +81,7 @@ const students = [
     homeworkText: "",
     homeworkLinkText: "Stuff we saw in our classes",
     fixedTime: "Wednesday (8:30PM to 9:30PM) and Saturday (1:00PM to 2:00PM)",
-    rescheduledClass: "Wednesday 6th at 7:30PM",
+    rescheduledClass: "1 pending class",
     paymentDue: "No payment due<br/><br/><strong>Monthly Payment Date:</strong> 30th",
     birthday: "1999-08-30",
     getHomeworkLink: function() {
@@ -96,7 +96,7 @@ const students = [
     homeworkText: "",
     homeworkLinkText: "Stuff we saw in our classes",
     fixedTime: "Tuesday (7:30PM to 8:30PM) and Thursday (6:30PM to 7:30PM)",
-    rescheduledClass: "[1 Pending Classes]",
+    rescheduledClass: "[3 Pending Classes]",
     paymentDue: "No payment due<br/><br/><strong>Monthly Payment Date:</strong> 10th",
     birthday: "1998-06-24",
     getHomeworkLink: function() {
